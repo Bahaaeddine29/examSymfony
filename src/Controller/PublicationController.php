fec9a39b53c639a2bb8766c393f024973b0bec14
Bahaaeddine <bahaaeddine.ghezz@gmail.com>
2023-08-03 12:24:38 +0200

    Ajout d'un commentaire pour test

--- src/Controller/PublicationController.php
+++ src/Controller/PublicationController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Publication;
+use App\Form\CommentaireType;
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -21,14 +25,45 @@ class PublicationController extends AbstractController
         ]);
     }
 
+    // #[Route('/show/{id}', name: 'app_show_publication')]
+    // public function showPublication(int $id, EntityManagerInterface $entityManager): Response
+    // {
+    //     $publication = $entityManager->find(Publication::class, $id);
+
+
+    //     return $this->render('publication/show.html.twig', [
+    //         'publication' => $publication,
+    //     ]);
+    // }
+
     #[Route('/show/{id}', name: 'app_show_publication')]
-    public function showPublication(int $id, EntityManagerInterface $entityManager): Response
+    public function showPublication(int $id, Request $request, EntityManagerInterface $entityManager): Response
     {
         $publication = $entityManager->find(Publication::class, $id);
 
+        
+        $commentaire = new Comment();
+        $commentForm = $this->createForm(CommentaireType::class, $commentaire);
+
+        $date = new DateTime();
+        $commentaire->setCreatedAt($date);
+
+        $commentForm->handleRequest($request);
+        if ($commentForm->isSubmitted() && $commentForm->isValid()) {
+            
+            $commentaire->setPublication($publication);
+
+            
+            $entityManager->persist($commentaire);
+            $entityManager->flush();
+
+            
+            return $this->redirectToRoute('app_show_publication', ['id' => $publication->getId()]);
+        }
 
         return $this->render('publication/show.html.twig', [
             'publication' => $publication,
+            'commentForm' => $commentForm->createView(), 
         ]);
     }
 }
